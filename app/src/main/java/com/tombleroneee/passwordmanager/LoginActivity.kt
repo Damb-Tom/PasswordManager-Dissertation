455f717b93c47780f3cb2832f7669096f147f1f7
Tom <tombleroneee@gmail.com>
2019-04-03 14:18:19 +0100

    Add:  - Login Authentication  - Email Verification  - Field Validation

--- app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
@@ -2,12 +2,83 @@ package com.tombleroneee.passwordmanager
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var user: FirebaseUser
+
+    private fun createToast(text: String, stayLength: Int) {
+        Toast.makeText(baseContext, text, stayLength).show()
+    }
+
+    private fun validateEmail(): Boolean {
+
+        user = auth.currentUser!!
+        var emailVerified = user.isEmailVerified
+        if (emailVerified) {
+            return true
+        }
+
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    createToast("Verification Email sent to ${user.email}", Toast.LENGTH_SHORT)
+                } else {
+                    createToast("Verification failed!", Toast.LENGTH_SHORT)
+                }
+            }
+        return emailVerified
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        forgotPasswordText.setOnClickListener {
+            var email = emailField.text.toString()
+            if (email.isEmpty()) {
+                createToast("Please enter an Email!", Toast.LENGTH_SHORT)
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "Password reset email has been sent!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(baseContext, "An account doesn't exist with that email!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+        }
+
+        login_button.setOnClickListener {
+            var email = emailField.text.toString()
+            val password = passwordField.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                createToast("Please enter an Email and Password!", Toast.LENGTH_SHORT)
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        if (validateEmail()) {
+                            createToast("Successfully Logged In!", Toast.LENGTH_SHORT)
+                        }
+                    } else {
+                        createToast("Invalid Username or Password, try again!", Toast.LENGTH_SHORT)
+                        passwordField.setText("")
+                    }
+                }
+        }
     }
 }
