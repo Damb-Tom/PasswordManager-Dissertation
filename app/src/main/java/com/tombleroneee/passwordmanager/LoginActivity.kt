c5883f850cadef3ae6b0690989e73b2410274020
Tom <tombleroneee@gmail.com>
2019-04-12 19:32:09 +0100

    Add:  - Auto sign in after registration functionality

--- app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.tombleroneee.passwordmanager
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -12,9 +14,34 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var user: FirebaseUser
+    private var stayLoggedIn: Boolean = false
 
     private fun createToast(text: String, stayLength: Int) {
         Toast.makeText(baseContext, text, stayLength).show()
+    }
+
+    private fun attemptLogin(email: String, password: String){
+        if (email.isEmpty() || password.isEmpty()) {
+            createToast("Please enter an Email and Password!", Toast.LENGTH_SHORT)
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            createToast("Invalid Email Address!", Toast.LENGTH_SHORT)
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    if (validateEmail()) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                } else {
+                    createToast("Invalid Username or Password, try again!", Toast.LENGTH_SHORT)
+                    passwordField.setText("")
+                }
+            }
     }
 
     private fun validateEmail(): Boolean {
@@ -41,6 +68,28 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        var settings = applicationContext.getSharedPreferences("USER_DATA", 0)
+        stayLoggedIn = settings.getBoolean("stayLoggedIn", false)
+        stay_logged_in_check.isChecked = stayLoggedIn
+
+        stay_logged_in_check.setOnCheckedChangeListener { _, isChecked ->
+            stayLoggedIn = isChecked
+
+            val editor = settings.edit()
+            editor.putBoolean("stayLoggedIn", stayLoggedIn)
+            editor.apply()
+        }
+
+        var registrationData = intent
+        var registrationEmail = "${registrationData.getStringExtra("email")}"
+        var registrationPassword = "${registrationData.getStringExtra("password")}"
+
+        if(registrationEmail != "null" && registrationPassword != "null"){
+            emailField.setText(registrationEmail)
+            passwordField.setText(registrationPassword)
+            attemptLogin(registrationEmail, registrationPassword)
+        }
+
         forgotPasswordText.setOnClickListener {
             var email = emailField.text.toString()
             if (email.isEmpty()) {
@@ -63,26 +112,7 @@ class LoginActivity : AppCompatActivity() {
             var email = emailField.text.toString()
             val password = passwordField.text.toString()
 
-            if (email.isEmpty() || password.isEmpty()) {
-                createToast("Please enter an Email and Password!", Toast.LENGTH_SHORT)
-                return@setOnClickListener
-            }
-            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                createToast("Invalid Email Address!", Toast.LENGTH_SHORT)
-                return@setOnClickListener
-            }
-
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        if (validateEmail()) {
-                            createToast("Successfully Logged In!", Toast.LENGTH_SHORT)
-                        }
-                    } else {
-                        createToast("Invalid Username or Password, try again!", Toast.LENGTH_SHORT)
-                        passwordField.setText("")
-                    }
-                }
+            attemptLogin(email, password)
         }
     }
 }
--- app/src/main/java/com/tombleroneee/passwordmanager/RegisterActivity.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/RegisterActivity.kt
@@ -1,13 +1,72 @@
 package com.tombleroneee.passwordmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.support.v7.app.AppCompatActivity
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private fun createToast(text: String, stayLength: Int) {
+        Toast.makeText(baseContext, text, stayLength).show()
+    }
+
+    private fun updateDisplayName(name: String){
+        val user = auth.currentUser
+        if(user != null) {
+            user.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(name).build())
+        }else{
+            Toast.makeText(baseContext, "Error adding a Display Name!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        auth = FirebaseAuth.getInstance()
+
+        registerButton.setOnClickListener {
+            var name = displayNameInput.text.toString()
+            var email = emailInputField.text.toString()
+            val password = passwordInputField.text.toString()
+            val passwordConfirm = passwordInputFieldConfirmation.text.toString()
+
+            if(password != passwordConfirm){
+                createToast("Passwords don't match!", Toast.LENGTH_SHORT)
+            }
+            if(name.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()){
+                createToast("Please enter a Display Name, Email and Password!", Toast.LENGTH_SHORT)
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                createToast("Invalid Email Address!", Toast.LENGTH_SHORT)
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        updateDisplayName(name)
+                       //createToast("Account has been created, please Login!", Toast.LENGTH_SHORT)
+
+                        val intent = Intent(this, LoginActivity::class.java).apply {
+                            putExtra("email", email)
+                            putExtra("password", password)
+                        }
+                        startActivity(intent)
+                    } else {
+                        var e = task.exception
+                        createToast("Error: ${e!!.message}", Toast.LENGTH_SHORT)
+                    }
+                }
+        }
     }
 }
