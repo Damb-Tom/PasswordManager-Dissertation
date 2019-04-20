3384df11c3ea888b96486537ea884518fa343bbf
Tom <tombleroneee@gmail.com>
2019-04-20 12:53:33 +0100

    Add:  - Added global data types

--- app/src/main/java/com/tombleroneee/passwordmanager/RecyclerData.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/RecyclerData.kt
@@ -1,3 +1,7 @@
 package com.tombleroneee.passwordmanager
 
-data class RecyclerData(val title: String, val username: String, val Password: String)
+import com.google.firebase.database.DatabaseReference
+
+data class RecyclerDataWithoutRef(val title: String, val username: String, val Password: String)
+data class RecyclerData(val title: String, val username: String, val Password: String, val urlRef: DatabaseReference, val usernameRef: DatabaseReference, val passwordRef: DatabaseReference)
+var recyclerListList = ArrayList<RecyclerData>()
