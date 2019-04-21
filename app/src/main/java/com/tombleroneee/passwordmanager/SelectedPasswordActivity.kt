40936d681a20ee02e9d5cd56ee81976b7413c2c9
Tom <tombleroneee@gmail.com>
2019-04-21 15:37:26 +0100

    Removed:  - Removed Sign out functionality from the selected password activity

--- app/src/main/java/com/tombleroneee/passwordmanager/SelectedPasswordActivity.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/SelectedPasswordActivity.kt
@@ -1,13 +1,8 @@
 package com.tombleroneee.passwordmanager
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.InputType
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_selected_password.*
 
 class SelectedPasswordActivity : AppCompatActivity() {
@@ -31,30 +26,5 @@ class SelectedPasswordActivity : AppCompatActivity() {
                 txtPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
             }
         }
-    }
-
-    private var backPressedTwice = false
-    override fun onBackPressed() {
-        if (backPressedTwice) {
-            val signOutDialogBox = AlertDialog.Builder(this)
-            signOutDialogBox.setTitle("Sign Out?")
-            signOutDialogBox.setMessage("Would you like to sign out?")
-
-            signOutDialogBox.setPositiveButton("Yes") { dialog, _ ->
-                dialog.dismiss()
-
-                Toast.makeText(baseContext, "Signed out successfully!", Toast.LENGTH_SHORT).show()
-                FirebaseAuth.getInstance().signOut()
-                startActivity(Intent(this@SelectedPasswordActivity, PreLoginActivity::class.java))
-            }
-
-            signOutDialogBox.setNeutralButton("No") { dialog, _ ->
-                dialog.cancel()
-            }
-            signOutDialogBox.show()
-            return
-        }
-        this.backPressedTwice = true
-        Handler().postDelayed({ this.backPressedTwice = false }, 2000)
     }
 }
