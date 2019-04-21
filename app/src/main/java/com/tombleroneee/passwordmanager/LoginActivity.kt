573c9931b9978842fc48c34665af835b88341c81
Tom <tombleroneee@gmail.com>
2019-04-21 15:05:43 +0100

    Bug:  - Added a TODO bug that needs fixing

--- app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
+++ app/src/main/java/com/tombleroneee/passwordmanager/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.tombleroneee.passwordmanager
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -114,5 +113,6 @@ class LoginActivity : AppCompatActivity() {
 
             attemptLogin(email, password)
         }
+        // TODO: PRESSING ENTER WHILE LOGGING IN WILL RESET YOUR PASSWORD..
     }
 }
